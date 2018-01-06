74963a2ce30bb5bc70f0a1f512735e4aa1d7042b
icarus213 <32793558+icarus213@users.noreply.github.com>
2018-01-06 10:05:41 +0700

    Update NeuralNetwork.py

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -2,7 +2,7 @@ from numpy import exp, array, random, dot, average, savez, load
 import sys
 
 if sys.version_info >= (3, 0):
-	xrange = range	# In python3 range is python2's xrange
+	xrange = range	# Python3's range() is python2's xrange()
 
 class NeuronLayer():
 	def __init__(self, neurons=0, inputs=0, predefined_weight=array(0)):
@@ -13,20 +13,21 @@ class NeuronLayer():
 			self.neurons = neurons
 			self.inputs = inputs
 		else:
-			self.synaptic_weight = predefined_weight 	# Load from a trained weights (load_weights is much preferred. Use this if you want to load weights for one layer only)
-														# The value of 'neurons' and 'inputs' variables are ignored if you load weights this way
+			# Load from a trained weights (load_weights is much preferred. Use this if you want to load weights for one layer only)
+			# The value of 'neurons' and 'inputs' variables are ignored if you load weights this way
+			self.synaptic_weight = predefined_weight
 			self.neurons = predefined_weight.shape[1]
 			self.inputs = predefined_weight.shape[0]
 
 
 class NeuralNetwork():
 	def __init__(self, layers):
-		previousValue = 0
+		previous = 0
 		for x in layers:
-			if previousValue != 0:
-				if len(previousValue.synaptic_weight[0]) != len(x.synaptic_weight):
+			if previous != 0:
+				if previous.neurons != x.inputs:
 					raise Exception('The number of inputs on a layer must be equal to the number of neurons on the previous layer')
-			previousValue = x
+			previous = x
 		self.layer = layers
 		self.total_layers = len(layers)
 		self.neurons = [x.neurons for x in layers]
@@ -46,7 +47,8 @@ class NeuralNetwork():
 			previousValue = output_from_layer[-1]
 		return output_from_layer
 
-	def train(self, training_set_input, training_set_output, number_of_training_iterations, tolerance=0, learning_rate=1, verbose=0):
+	def train(self, training_set_input, training_set_output, number_of_training_iterations, tolerance=0, learning_rate=1, verbose=0, steps=1):
+		steps = xrange(0,number_of_training_iterations,steps)
 		if len(training_set_input) == len(training_set_output):
 			for iteration in xrange(number_of_training_iterations):
 				output_from_layer = self.think(training_set_input)
@@ -66,9 +68,12 @@ class NeuralNetwork():
 					self.layer[x].synaptic_weight += learning_rate * layer_adjustment[x]
 				sum_error = average(sum(array(layer_error[0])**2)/len(training_set_output))
 				if sum_error < tolerance:
+					# Breaks the loop if sum_error is less than tolerance
 					break
 				if verbose:
-					print('>epoch=%d, learning rate=%.3f, error=%.3f' % (iteration, learning_rate, 100*sum_error) + '%')
+					if iteration in steps:
+						# Prints information about the training process
+						print('>epoch=%d, learning rate=%.3f, error=%.3f' % (iteration, learning_rate, 100*sum_error) + '%')
 		else:
 			raise Exception('The number of training_set_input must be equal with the number of training_set_output')
 
@@ -91,10 +96,12 @@ class NeuralNetwork():
 		if sys.version_info >= (3, 0):
 			archive = load(file_name,encoding='latin1')
 		else:
-			archive = load(file_name) # Load the archive
+			# Load the archive
+			archive = load(file_name)
 		c = []
 		for x in reversed(archive.files):
-			c.append(archive[x]) # Load the weights from the files in the archive
+			# Load the weights from the files in the archive
+			c.append(archive[x])
 		if len(c) == self.total_layers:
 			for x in xrange(len(c)):
 				if c[x].shape == self.layer[x].synaptic_weight.shape:
@@ -103,11 +110,13 @@ class NeuralNetwork():
 					raise Exception('Different neural network configuration received from file:' + '\n'
 						+ 'NeuralNetwork configuration: Layer %d: Neuron %d Input %d' % (x,self.neurons[x],self.inputs[x]) + '\n'
 						+ 'NeuralNetwork configuration from file: Layer %d: Neuron %d Input %d' % (x,c[x].shape[1],c[x].shape[0]))
+		else:
 			raise Exception('The number of layers must match with the number of configured layers in the NeuralNetwork' + '\n'
 			 + 'The number of layers in file: %d. The number of layers in the NeuralNetwork: %d' % (len(c),self.total_layers))
 
 	def save_weights(self, file_name):
-		savez(file_name,*self.get_weights()) # Save the weights to an archive
+		# Save the weights to an archive
+		savez(file_name,*self.get_weights())
 
 	@staticmethod
 	def from_file(file_name):
@@ -116,8 +125,10 @@ class NeuralNetwork():
 		if sys.version_info >= (3, 0):
 			archive = load(file_name,encoding='latin1')
 		else:
-			archive = load(file_name) # Load the archive
+			# Load the archive
+			archive = load(file_name)
 		layers = []
 		for x in reversed(archive.files):
-			layers.append(NeuronLayer(archive[x])) # Load the weights from the files in the archive
+			# Load the weights from the files in the archive
+			layers.append(NeuronLayer(archive[x]))
 		return NeuralNetwork(layers)
