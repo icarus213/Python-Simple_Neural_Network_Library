59afae605021fb56e58663130ef2dcb03778f7d2
icarus213 <32793558+icarus213@users.noreply.github.com>
2018-01-06 09:34:07 +0700

    Update NeuralNetwork.py

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,26 +1,23 @@
-from numpy import exp, array, random, dot, average, save, load
+from numpy import exp, array, random, dot, average, savez, load
 import sys
 
 if sys.version_info >= (3, 0):
-	xrange = range
+	xrange = range	# In python3 range is python2's xrange
 
 class NeuronLayer():
-	def __init__(self, neurons, inputs, predefined_weight=array(0)):
-		if predefined_weight.all() == 0:
-			self.synaptic_weight = 2*random.random([inputs, neurons])
-		else: self.synaptic_weight = predefined_weight
+	def __init__(self, neurons=0, inputs=0, predefined_weight=array(0)):
+		if predefined_weight.all() == 0: 
+			assert neurons != 0
+			assert inputs != 0
+			self.synaptic_weight = 2*random.random([inputs, neurons]) # Generate random weights
+			self.neurons = neurons
+			self.inputs = inputs
+		else:
+			self.synaptic_weight = predefined_weight 	# Load from a trained weights (load_weights is much preferred. Use this if you want to load weights for one layer only)
+														# The value of 'neurons' and 'inputs' variables are ignored if you load weights this way
+			self.neurons = predefined_weight.shape[1]
+			self.inputs = predefined_weight.shape[0]
 
-from numpy import exp, array, random, dot, average, save, load
-import sys
-
-if sys.version_info >= (3, 0):
-	xrange = range
-
-class NeuronLayer():
-	def __init__(self, neurons, inputs, predefined_weight=array(0)):
-		if predefined_weight.all() == 0:
-			self.synaptic_weight = 2*random.random([inputs, neurons])
-		else: self.synaptic_weight = predefined_weight
 
 class NeuralNetwork():
 	def __init__(self, layers):
@@ -31,6 +28,9 @@ class NeuralNetwork():
 					raise Exception('The number of inputs on a layer must be equal to the number of neurons on the previous layer')
 			previousValue = x
 		self.layer = layers
+		self.total_layers = len(layers)
+		self.neurons = [x.neurons for x in layers]
+		self.inputs = [x.inputs for x in layers]
 
 	def __sigmoid(self, x, deriv=False):
 		if(deriv):
@@ -64,11 +64,11 @@ class NeuralNetwork():
 					previousValue = output_from_layer[x]
 				for x in xrange(len(self.layer)):
 					self.layer[x].synaptic_weight += learning_rate * layer_adjustment[x]
-				sum_error = average(sum([(training_set_output[i]-output_from_layer[-1][i])**2 for i in xrange(len(training_set_output))])/len(training_set_output))
+				sum_error = average(sum(array(layer_error[0])**2)/len(training_set_output))
 				if sum_error < tolerance:
 					break
 				if verbose:
-					print('>epoch=%d, learning rate=%.3f, error=%.3f' % (iteration, learning_rate, 100*sum_error))
+					print('>epoch=%d, learning rate=%.3f, error=%.3f' % (iteration, learning_rate, 100*sum_error) + '%')
 		else:
 			raise Exception('The number of training_set_input must be equal with the number of training_set_output')
 
@@ -83,23 +83,41 @@ class NeuralNetwork():
 		output = []
 		for x in self.layer:
 			output.append(x.synaptic_weight)
-		return array(output)
+		return output
 
 	def load_weights(self,file_name):
+		if '.npz' != file_name[-4:]:
+			file_name += '.npz'
 		if sys.version_info >= (3, 0):
-			c = load(file_name,encoding='latin1')
+			archive = load(file_name,encoding='latin1')
 		else:
-			c = load(file_name)
-		if len(c) == len(self.layer):
+			archive = load(file_name) # Load the archive
+		c = []
+		for x in reversed(archive.files):
+			c.append(archive[x]) # Load the weights from the files in the archive
+		if len(c) == self.total_layers:
 			for x in xrange(len(c)):
-				if len(c[x]) == len(self.layer[x].synaptic_weight):
+				if c[x].shape == self.layer[x].synaptic_weight.shape:
 					self.layer[x].synaptic_weight = array(c[x])
 				else:
-					raise Exception('The number of weights must match with the number of configured weights on layer ' + str(x) + '\n'
-					 + 'The number of weights in file: ' + str(len(c[x])) + '. The number of configured weights: ' + str(len(self.layer[x].synaptic_weight)))
-		else:
+					raise Exception('Different neural network configuration received from file:' + '\n'
+						+ 'NeuralNetwork configuration: Layer %d: Neuron %d Input %d' % (x,self.neurons[x],self.inputs[x]) + '\n'
+						+ 'NeuralNetwork configuration from file: Layer %d: Neuron %d Input %d' % (x,c[x].shape[1],c[x].shape[0]))
 			raise Exception('The number of layers must match with the number of configured layers in the NeuralNetwork' + '\n'
-			 + 'The number of layers in file: ' + str(c) + 'The number of layers in the NeuralNetwork: ' + str(len(self.layer)))
+			 + 'The number of layers in file: %d. The number of layers in the NeuralNetwork: %d' % (len(c),self.total_layers))
 
 	def save_weights(self, file_name):
-		save(file_name,self.get_weights())
+		savez(file_name,*self.get_weights()) # Save the weights to an archive
+
+	@staticmethod
+	def from_file(file_name):
+		if '.npz' != file_name[-4:]:
+			file_name += '.npz'
+		if sys.version_info >= (3, 0):
+			archive = load(file_name,encoding='latin1')
+		else:
+			archive = load(file_name) # Load the archive
+		layers = []
+		for x in reversed(archive.files):
+			layers.append(NeuronLayer(archive[x])) # Load the weights from the files in the archive
+		return NeuralNetwork(layers)
